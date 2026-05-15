4fb7c8b134f8f2274ab8c72774cfa3dc535a9841
AminBadalzade <amin.badalzade@sa.stud.vu.lt>
2026-05-15 18:26:04 +0300

    Initial commit

---
+++ src/test/java/com/amin/advancemapping/AdvancemappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amin.advancemapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancemappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
